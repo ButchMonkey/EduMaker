e09f46faf5d9f9154f2534bcf24e88c76000cdeb
ButchMonkey <14085141@i.ocvc.ac.uk>
2015-12-17 13:59:36 +0000

    Removed Redundent Code. v1.1.1

--- LightFunctions.ino
+++ LightFunctions.ino
@@ -38,12 +38,6 @@ target target;
 void fadeColour(int, int, int);
 
 void setup() {
-  // This is for Trinket 5V 16MHz, you can remove these three lines if you are not using a Trinket
-  #if defined (__AVR_ATtiny85__)
-    if (F_CPU == 16000000) clock_prescale_set(clock_div_1);
-  #endif
-  // End of trinket special code
-
   strip.begin();
   strip.show(); // Initialize all pixels to 'off'
 }
